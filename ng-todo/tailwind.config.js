451432e0241e7dd5afe5c35f4d1da0a865c6d363
AZ <47402666+realazaber@users.noreply.github.com>
2024-04-11 21:44:19 +0200

    Chore: Tailwind config setup

--- ng-todo/tailwind.config.js
+++ ng-todo/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#dc0031",
+      },
+    },
   },
   plugins: [],
-}
+};

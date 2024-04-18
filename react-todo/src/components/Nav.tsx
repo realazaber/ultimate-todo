3627760e1ec6d4b59dccc3dc1990e723f664da8a
AZ <47402666+realazaber@users.noreply.github.com>
2024-04-18 20:28:47 +0200

    Feature: Nav component

---
+++ react-todo/src/components/Nav.tsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+  return (
+    <nav className="fixed top-0 right-0 left-0 bg-white p-3">
+      <div>
+        <span className="text-primary font-extrabold">Ng Todo</span>
+      </div>
+    </nav>
+  );
+}
